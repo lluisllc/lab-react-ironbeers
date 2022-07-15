11de1382cd1a64ab73721ac838f34611dff6ae68
lluisllc <lluis.llacera@gmail.com>
2022-07-15 13:13:12 +0200

    Imagen OK

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,22 @@
 import React from "react";
+import beer1 from "../assets/beers.png"
+import beer2 from "../assets/beers.png"
+import beer3 from "../assets/beers.png"
 
 
 function Home() {
     return (
         <div>
-            <img src="../assets/beers.png" alt='Beers' />
+            <img src={beer1} alt='Beers' />
+            <br></br>
             <a href="/beers">Beers List</a>
             <p>Lorem ipsum 1</p>
-            <img src="../assets/random-beer.png.png" alt='rndBeers' />
+
+            <img src="../assets/random-beer.png" alt='rndBeers' />
             <a href="/random-beer">Get a random beer</a>
             <p>Lorem ipsum 2</p>
-            <img src="../assets/new-beer.png" alt='Beers' />
+
+            <img src="../assets/new-beer.png" alt='newBeer' />
             <a href="/new-beer">Send a new beer!</a>
             <p>Lorem ipsum 3</p>
         </div>
